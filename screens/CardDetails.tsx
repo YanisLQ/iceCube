a780a670754258879425993ab286566dbec9ed8a
Leclercq Yanis <leclercqyanis@gmail.com>
2023-05-20 02:58:14 +0200

    carddetails : ok

--- screens/CardDetails.tsx
+++ screens/CardDetails.tsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { db, auth } from '../firebase-config';
+import { getFirestore, collection, getDocs, setDoc, doc, getDoc, query, where } from "firebase/firestore";
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function CardDetails({navigation}: RootStackScreenProps<'CardDetails'>) {
+  const [categories, setCategories] = useState([]);
+  const [activeCategory, setActiveCategory] = useState('');
+  const [card, setCards] = useState([])
+  const [filteredCards, setFilteredCards] = useState([]);
+
   const route = useRoute();
-  console.log(route.params)
   const { id } = route.params;
 
   const goBack = () => {
     navigation.goBack();
   };
+
+  useEffect(() => {
+    // Récupérer les catégories depuis la collection "plats" dans Firebase
+    const fetchCategories = async () => {
+      try {
+        const q = query(collection(db, "plats"), where("categoriesId", "==", id));
+        const querySnapshot = await getDocs(q);
+        const categoriesData = querySnapshot.docs.map(doc => doc.data().souscat);
+        const mapCard = querySnapshot.docs.map(doc => doc.data());
+        setCards(mapCard)
+        setCategories(categoriesData);
+        setActiveCategory(categoriesData[0]); // Définir la première catégorie comme active par défaut
+      } catch (error) {
+        console.log('Erreur lors de la récupération des catégories :', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+  useEffect(() => {
+    const filterCardsByCategory = () => {
+      const filtered = card.filter((card) => card.souscat === activeCategory);
+      console.log(filtered)
+      setFilteredCards(filtered);
+    };
+    filterCardsByCategory();
+  }, [activeCategory]);
+  const handleCategoryPress = (category) => {
+    setActiveCategory(category);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-        <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 24}}>
+        <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 24}}>
           <TouchableOpacity onPress={goBack} style={{zIndex: 1}}>
             <Image source={require('../assets/images/backArrow2.png')} style={{width: 24, height: 24, marginLeft: 22}} />     
           </TouchableOpacity>
@@ -29,11 +65,33 @@ export default function CardDetails({navigation}: RootStackScreenProps<'CardDeta
         </View>
       </View>
       <ScrollView>
-        {/* Afficher les détails de la carte en fonction de l'ID */}
-        <TouchableOpacity style={styles.card}>
-          <Text style={styles.cardTitle}>dijdskjd</Text>
-        </TouchableOpacity>
-        {/* Ajoutez d'autres cartes ou contenu ici */}
+        <View style={styles.menu}>
+          {categories.map((category) => (
+            <TouchableOpacity
+              key={category}
+              style={[styles.menuButton, activeCategory === category && styles.activeMenuButton]}
+              onPress={() => handleCategoryPress(category)}
+            >
+              <Text style={[styles.menuButtonText, activeCategory === category && styles.activeMenuButtonText]}>{category}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={styles.cardsContainer}>
+          {filteredCards.map((card) => (
+            <TouchableOpacity key={card.id} style={styles.card}>
+              <Text style={styles.cardTitle}>{card.nom}</Text>
+              <View style={styles.tableContainer}>
+              {card.ingredients.map((ingredient, index) => (
+                <Text key={index} style={styles.ingredientText}>{ingredient} </Text>
+              ))}
+              </View>
+              <View style={styles.bottomCards}>
+                <Text style={styles.priceTxt}>{card.prix}€</Text>
+                <Image source={require('../assets/images/CardDetails/fleche.png')} />
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -53,10 +111,8 @@ const styles = StyleSheet.create({
     fontFamily: 'Inter_600SemiBold',
   },
   header: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#E5E5E5',
-    paddingVertical: 10,
     paddingHorizontal: 16,
+    marginVertical: 15
   },
   helloContainer: {
     flexDirection: 'row',
@@ -68,18 +124,82 @@ const styles = StyleSheet.create({
     lineHeight: 27,
     marginLeft: 8
   },
-  card: {
-    width: windowWidth,
-    height: 200,
-    backgroundColor: '#FBBFB8',
-    borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
+  menu: {
+    flexDirection: 'row',
     marginBottom: 16,
+    paddingHorizontal: 16,
+    justifyContent: 'space-evenly',
   },
-  cardTitle: {
+  menuButton: {
+    marginRight: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    // backgroundColor: '#FBBFB8',
+  },
+  activeMenuButton: {
+    // backgroundColor: '#FFDFDB',
+    borderBottomWidth: 2,
+    borderColor: '#FFDFDB'
+  },
+  menuButtonText: {
     fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center',
+    color: '#000',
+    textTransform: 'capitalize'
   },
+  activeMenuButtonText: {
+    color: '#FF5753',
+  },
+  cardsContainer: {
+    paddingHorizontal: 22,
+  },
+  card: {
+    width: '100%',
+    height: windowHeight * 0.20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 12,
+    marginBottom: 16,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5,
+    paddingLeft: 18,
+  },
+  cardTitle: {
+    paddingTop: 14,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    fontFamily: 'Inter_500Medium'
+  },
+  tableContainer: {
+    flexDirection: 'row', 
+    textTransform: 'capitalize',
+  },
+  ingredientText: {
+    fontFamily: 'Inter_400Regular',
+    paddingTop: 8,
+    fontSize: 12
+  },
+  bottomCards:{
+    position:'absolute',
+    bottom: 18,
+    left: 18,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: "space-between",
+    // backgroundColor: 'red',
+    width: '100%',
+    paddingRight: 16
+  },
+  priceTxt:{
+    fontFamily: 'Inter_600SemiBold',
+    fontSize: 16,
+    color: '#F0604D'
+  }
 });
